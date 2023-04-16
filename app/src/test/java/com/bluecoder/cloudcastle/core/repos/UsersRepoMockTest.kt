2be8f86a3e6f4b5470208b480f68588340c7e64d
= <=>
2023-04-16 19:03:04 +0000

    Add sign up with valid user test and login with invalid username test

--- app/src/test/java/com/bluecoder/cloudcastle/core/repos/UsersRepoMockTest.kt
+++ app/src/test/java/com/bluecoder/cloudcastle/core/repos/UsersRepoMockTest.kt
@@ -14,13 +14,17 @@ import org.junit.Test
 class UsersRepoMockTest{
     private lateinit var repo : UsersRepoMock
 
+    private val validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODE0MTgzMjEsImRhdGEiOnsiaWQiOjMsInVzZXJuYW1lIjoidXNlcjIifSwiaWF0IjoxNjgxNDE2MjIxfQ.FrF6qw730u1SMAIWy0PsY4YzxJG-a-UWPBpwQuvO0_U"
+
+
     @Before
     fun setup(){
         repo = UsersRepoMock()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `test duplicate username`() = runTest {
+    fun `test duplicate username when signup`() = runTest {
 
         var result : Result<UserJWT>? = null
 
@@ -36,4 +40,23 @@ class UsersRepoMockTest{
 
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `test successful signup`() = runTest {
+         repo.signup(User("user1","123")).collectLatest {
+             assertTrue(it.isSuccess)
+             assertTrue(it.getOrNull() is UserJWT)
+             assertEquals(it.getOrNull()?.token,validToken)
+         }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `test login with invalid username`() = runTest {
+        repo.login(User("user2","123")).collectLatest {
+            assertTrue(it.isFailure)
+            assertTrue(it.exceptionOrNull() is Exception)
+            assertEquals("User not found, please enter a valid username",it.exceptionOrNull()?.message)
+        }
+    }
 }
